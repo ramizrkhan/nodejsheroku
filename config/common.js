ec2cf7c59a4d54e616a096c92fb86ec733497c98
ramizrkhan <ramizrazakhan@hotmail.com>
2018-09-10 15:53:17 +0530

    removed  security  Content-Security-Policy

--- config/common.js
+++ config/common.js
@@ -4,7 +4,7 @@ function security(res){
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
 res.header('Expires', '-1');
 res.header('Pragma', 'no-cache');
-res.header("Content-Security-Policy", "default-src 'self';script-src 'self';object-src 'none';img-src 'self';media-src 'self';frame-src 'none';font-src 'self' data:;connect-src 'self';style-src 'self'");
+// res.header("Content-Security-Policy", "default-src 'self';script-src 'self';object-src 'none';img-src 'self';media-src 'self';frame-src 'none';font-src 'self' data:;connect-src 'self';style-src 'self'");
 
 return res;
 }
